50ce2343de302bd67735d7e6d1267d62aaa2aade
Priyank Mandavawala <primanda@microsoft.com>
2024-03-25 15:29:43 +0530

    Refactor code in main.rs to use flate2::read::ZlibDecoder instead of flate2::read::GzDecoder

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use flate2::read::ZlibDecoder;
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::io::Read;
-use flate2::read::ZlibDecoder;
 
 use clap::{Parser, Subcommand};
 
@@ -18,7 +18,7 @@ struct Args {
 enum Command {
     Init,
     CatFile {
-        #[clap(short = 'p' )]
+        #[clap(short = 'p')]
         pretty_print: bool,
 
         object_hash: String,
@@ -29,35 +29,44 @@ fn main() {
     let args = Args::parse();
 
     match args.command {
-        Command::Init=> {
-            fs::create_dir(".git").unwrap();
-            fs::create_dir(".git/objects").unwrap();
-            fs::create_dir(".git/refs").unwrap();
-            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-            println!("Initialized git directory")
-        },
-        Command::CatFile { pretty_print, object_hash } => {
-            //get the first two characters of the hash
-            let prefix = &object_hash[0..2];
-            //get the rest of the hash
-            let suffix = &object_hash[2..];
-            //get the file path
-            let file_path = format!(".git/objects/{}/{}", prefix, suffix);
-            //read the file
-            let contents = fs::read(file_path).unwrap();
-            //decompress the file
-            let mut decoded_data_decoder =  ZlibDecoder::new(contents.as_slice());
-            let mut decoded_data = String::new();
-            decoded_data_decoder.read_to_string(&mut decoded_data).unwrap();
+        Command::Init => init(),
+        Command::CatFile {
+            pretty_print,
+            object_hash,
+        } => cat_file(&object_hash, pretty_print),
+    }
+}
 
-            //remove the header from the decoded data
-            let decoded_data = decoded_data.splitn(2, '\0').collect::<Vec<&str>>()[1];
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory")
+}
 
-            if pretty_print {
-                print!("{}", decoded_data);
-            } else {
-                print!("{:?}", decoded_data);
-            }
-        }
+fn cat_file(object_hash: &str, pretty_print: bool) {
+    //get the first two characters of the hash
+    let prefix = &object_hash[0..2];
+    //get the rest of the hash
+    let suffix = &object_hash[2..];
+    //get the file path
+    let file_path = format!(".git/objects/{}/{}", prefix, suffix);
+    //read the file
+    let contents = fs::read(file_path).unwrap();
+    //decompress the file
+    let mut decoded_data_decoder = ZlibDecoder::new(contents.as_slice());
+    let mut decoded_data = String::new();
+    decoded_data_decoder
+        .read_to_string(&mut decoded_data)
+        .unwrap();
+
+    //remove the header from the decoded data
+    let decoded_data = decoded_data.splitn(2, '\0').collect::<Vec<&str>>()[1];
+
+    if pretty_print {
+        print!("{}", decoded_data);
+    } else {
+        print!("{:?}", decoded_data);
     }
 }
