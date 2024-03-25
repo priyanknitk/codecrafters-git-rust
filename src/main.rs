ddc7cf255530eb513b59ab9c51f3dd7deb9aeb22
Priyank Mandavawala <primanda@microsoft.com>
2024-03-25 15:23:28 +0530

    Add CatFile command to main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
+use flate2::read::GzDecoder;
 
 use clap::{Parser, Subcommand};
 
@@ -15,6 +17,12 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short = 'p' )]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
 }
 
 fn main() {
@@ -27,6 +35,26 @@ fn main() {
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
+        },
+        Command::CatFile { pretty_print, object_hash } => {
+            //get the first two characters of the hash
+            let prefix = &object_hash[0..2];
+            //get the rest of the hash
+            let suffix = &object_hash[2..];
+            //get the file path
+            let file_path = format!(".git/objects/{}/{}", prefix, suffix);
+            //read the file
+            let contents = fs::read(file_path).unwrap();
+            //decompress the file
+            let mut decoded_data_decoder =  GzDecoder::new(contents.as_slice());
+            let mut decoded_data = String::new();
+            decoded_data_decoder.read_to_string(&mut decoded_data).unwrap();
+
+            if pretty_print {
+                println!("{}", decoded_data);
+            } else {
+                println!("{:?}", decoded_data);
+            }
         }
     }
 }
